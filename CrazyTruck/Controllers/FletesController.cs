e2222c47875efeb03ebe98d7b661b8008fbd0c17
manuellozano <isw.manuellozano@gmail.com>
2018-03-08 18:44:51 -0700

    primer commit

---
+++ CrazyTruck/Controllers/FletesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrazyTruck.Controllers
+{
+    public class FletesController : Controller
+    {
+        // GET: Fletes
+        public ActionResult Fletes()
+        {
+            return View();
+        }
+    }
+}
